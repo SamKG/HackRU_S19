a90618a8521167f8a0fb7c538e7cfcdc4436e1d6
samkg <samkg@nbp-67-133.nbp.ruw.rutgers.edu>
2019-03-09 20:51:07 -0500

    adapted algo code

--- algo_plus_chat/chat_putter.py
+++ algo_plus_chat/chat_putter.py
@@ -3,12 +3,12 @@ import ast
 import pprint
 
 def get_chat(video_id):
-
-	vid = bytes(video_id, encoding = 'utf - 8')
-	pipes = subprocess.Popen(["python", "app.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-	std_out, std_err = pipes.communicate(input=vid)
-
-	out = eval(std_err)
-	# pprint.pprint(out)
-
-	return out
+    vid = bytes(video_id, encoding = 'utf - 8')
+    pipes = subprocess.Popen(["python3", "app.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    #print("STEP 1")
+    std_out, std_err = pipes.communicate(input=vid)
+    #print("STEP 2")
+    out = eval(std_err)
+    #print("STEP 3")
+    # pprint.pprint(out)
+    return out
--- algo_plus_chat/main.py
+++ algo_plus_chat/main.py
@@ -1,14 +1,16 @@
 from time_list_generator import get_timestamps
 from chat_putter import get_chat
+from argparse import ArgumentParser
 
-video_id = "391828640"
+def get_vid_data(video_id=377499328):
+    chat_log = get_chat(video_id)
 
-chat_log = get_chat(video_id)
+    # for chat in chat_log:
+    # 	print(chat)
 
-# for chat in chat_log:
-# 	print(chat)
+    timestamp_list = get_timestamps(chat_log)
+    return timestamp_list
 
-timestamp_list = get_timestamps(chat_log)
-
-for i, j in timestamp_list:
-	print(("%s %s" %(i, j)))
+if __name__ == '__main__':
+    for t in get_vid_data():
+        print(t,'\n')
