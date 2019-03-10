8c227a37ffdfc74b289b7013000bbddb33a4d231
samkg <samkg@nbp-67-133.nbp.ruw.rutgers.edu>
2019-03-10 01:29:51 -0500

    Revert "push"
    This reverts commit d1b5d64c9cc40014d71398f7e1e1f96e6e0cd5e7.

--- twitchfreq_server/twitchfreq_server/views.py
+++ twitchfreq_server/twitchfreq_server/views.py
@@ -1,15 +1,11 @@
 from django.http import HttpResponse,JsonResponse
 from django.template import Context,loader
-from .lib.algo_plus_chat.main import get_vid_data
-
 def index(request):
     templ = loader.get_template('index.html')
-    return HttpResponse(templ.render(request=request))
+    return HttpResponse(templ.render())
     #return render_to_response('templates/index.html')
 
 def highlight(request,video_id = -1):
     if video_id == -1:
         return JsonResponse({'values':[]})
-    print("OBTAINING INFO FOR "+str(video_id))
-    timestamps = get_vid_data(str(video_id))
-    return JsonResponse(timestamps)
+    return JsonResponse({'values':[]})
